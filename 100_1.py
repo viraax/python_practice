21d66a9e9aa067e3822f95ee40b2614dcc871f96
Petia Nikolova <viraax@Petias-MacBook-Pro.local>
2018-07-14 17:36:20 +0300

    1 first commit

---
+++ 100_1.py
@@ -0,0 +1,20 @@
+
+def main():
+
+	print("\n")	
+	name = input("What is your name? ")
+	age = int(input("Hey {0}! How old are you? ".format(name)))
+
+	while ((age < 1) or (age > 125)):
+		print("That was invalid! Try again!")
+		age = int(input("Hey {0}! How old are you? ".format(name)))
+
+	repeat = int(input("Enter a number more than 1 & up to 100 "))
+	while ((repeat < 1) or (repeat > 100)):
+		repeat = int(input("Enter a number more than 1 & up to 100 "))
+
+	years_left = (100-age)
+	for i in range(0, repeat):
+		print("You will turn 100 in about", years_left, "years! \n")
+	
+main()
